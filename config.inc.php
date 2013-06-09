93b453ad3d60e3687696008cd2da776923f1fe84
cyndiWade <zhanglin492103904@qq.com>
2013-06-09 10:49:22 +0800

    six

---
+++ config.inc.php
@@ -0,0 +1,17 @@
+<?php 
+if (!defined('THINK_PATH'))	exit();
+
+return array (
+	/* 数据库配置 */
+    'DB_TYPE'               => 'mysql',    
+	'DB_HOST'               => 'localhost',
+	'DB_NAME'               => 'sq_weixin123',      
+	'DB_USER'               => 'sq_weixin123',    
+	'DB_PWD'                => 'asdfasdf',  
+	'DB_PORT'               => '3306',  
+	'DB_PREFIX'             => 'oa_',  
+    'DB_FIELDTYPE_CHECK'    => false,    
+    'DB_FIELDS_CACHE'       => true,       
+    'DB_CHARSET'            => 'utf8',     
+);
+?>
